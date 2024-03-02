588fb1b54802af593f86bea2b3ad3fb576641398
Saumik Satapathy <oracle.saumik@gmail.com>
2024-03-02 15:52:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,6 @@ provider "digitalocean" {
 
 variable "prod" {
   description = "Path to the public key file for the prod environment"
-  default     = secrets.SSH_KEY
 }
 
 resource "digitalocean_ssh_key" "ssh_key" {
